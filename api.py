b50d34655d485a7ee01f07c7ce9704d77e639dba
Ifran Rahman Nijhum <52383588+ifran-rahman@users.noreply.github.com>
2022-11-03 01:08:08 +0600

    create api with frontend

--- api.py
+++ api.py
@@ -5,11 +5,11 @@ import pandas as pd
 import numpy as np
 import pickle 
 import streamlit as st
+import joblib
 
 app=Flask(__name__)
 
-pickle_in = open("regressor.pkl","rb")
-regressor = pickle.load(pickle_in)
+# regressor = joblib.load("xgb_reg.sav")
 
 
 def predict_note_authentication(variance, skewness, curtosis, entropy):
@@ -44,23 +44,24 @@ def predict_note_authentication(variance, skewness, curtosis, entropy):
 
 def main():
     st.title("Order quantity prediction")
-    html_temp = """
-    <div style = "background-color:tomato;padding:10px">
-    <h2 style="color:white;text-align:center;">Streamlit Bank Authenticator ML App </h2>
-    </div>
-    """
-    st.markdown(html_temp,unsafe_allow_html=True)
-    variance = st.text_input("Variance","Type Here")
-    skewness = st.text_input("skewness","Type Here")
-    curtosis = st.text_input("curtosis","Type Here")
-    entropy = st.text_input("entropy","Type Here")
-    result=""
-    if st.button("Predict"):
-        result=predict_note_authentication(variance,skewness,curtosis,entropy)
-    st.success('The output is {}'.format(result))
-    if st.button("About"):
-        st.text("Lets Laarn")
-        st.text("Built with Streamlit")
+    # html_temp = """
+    # <div style = "background-color:tomato;padding:10px">
+    # <h2 style="color:white;text-align:center;">Streamlit Bank Authenticator ML App </h2>
+    # </div>
+    # """
+    # st.markdown(html_temp,unsafe_allow_html=True)
+    # variance = st.text_input("Customer Since","Enter number of days")
+    # skewness = st.text_input("Age","Enter age")
+    # curtosis = st.text_input("Price","Enter pcoroduct price")
+    # entropy = st.text_input("Discount Percentage","Enter discount percentage")
+    
+    # result=""
+    # if st.button("Predict"):
+    #     result=predict_note_authentication(variance,skewness,curtosis,entropy)
+    # st.success('The output is {}'.format(result))
+    # if st.button("About"):
+    #     st.text("Lets Laarn")
+    #     st.text("Built with Streamlit")
 
 
 if __name__=='__main__':
